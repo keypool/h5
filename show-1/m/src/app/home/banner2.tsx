e47fc647aa18a3556355b6651bcd12612b72f14e
fay <869770665@qq.com>
2020-08-06 14:43:18 +0800

    upd

--- show-1/m/src/app/home/banner2.tsx
+++ show-1/m/src/app/home/banner2.tsx
@@ -20,38 +20,38 @@ export default () => {
         <Grid container>
           <Grid item xs={12}>
             <Title text={'Keystore'}/>
-            <Typography variant={"body2"} style={{marginTop: '20px'}}>企业级加密资产服务商</Typography>
-            <Typography variant={"body2"}>安全、高效、专业</Typography>
-            <Typography variant={"body2"} style={{marginTop: '20px'}}>Institutional-grade Crypto</Typography>
-            <Typography variant={"body2"}>Asset Service Provider</Typography>
+            <Typography variant={"body2"} style={{marginTop: '20px'}}>企&nbsp;业&nbsp;级&nbsp;加&nbsp;密&nbsp;资&nbsp;产&nbsp;服&nbsp;务&nbsp;商</Typography>
+            <Typography variant={"body2"}>Institutional-grade Crypto Asset Service Provider</Typography>
+            <Typography variant={"body2"} style={{marginTop: '20px'}}>安全、高效、专业</Typography>
           </Grid>
           <Grid container item xs={12} spacing={1} style={{height: 140}}>
-            <Grid container item xs={6} justify="flex-start" alignItems="center">
+            <Grid container item xs={4} justify="flex-start" alignItems="center">
               <ViewGrow>
-                <img width={'80%'} src={`https://oss.faycz.com/keypool/kps1m/product-1.png`}/>
+                <img width={'120%'} src={`https://oss.faycz.com/keypool/kps1m/product-1.png`}/>
               </ViewGrow>
             </Grid>
-            <Grid container item xs={6} justify="center" alignItems="center">
+            <Grid container item xs={8} justify="center" alignItems="center">
               <ViewGrow>
                 <div>
-                  <Typography>加密资产网银</Typography>
+                  <Typography variant={"body2"}>加密资产网银</Typography>
                   <Typography style={{fontSize: '0.75rem'}}>Keystore Enterprise</Typography>
                 </div>
               </ViewGrow>
             </Grid>
           </Grid>
           <Grid container item xs={12} spacing={1} style={{height: 140}}>
-            <Grid container item xs={6} justify="center" alignItems="center">
+            <Grid container item xs={8} justify="center" alignItems="center">
               <ViewGrow>
                 <div>
-                  <Typography>Filecoin矿池资产安全方案</Typography>
-                  <Typography style={{fontSize: '0.75rem'}}>Filecoin Mining Pool Asset Security Solutions</Typography>
+                  <Typography variant={"body2"}>Filecoin矿池资产安全方案</Typography>
+                  <Typography style={{fontSize: '0.75rem'}}>Filecoin Mining Pool</Typography>
+                  <Typography style={{fontSize: '0.75rem'}}>Asset Security Solutions</Typography>
                 </div>
               </ViewGrow>
             </Grid>
-            <Grid container item xs={6} justify="flex-end" alignItems="center">
+            <Grid container item xs={4} justify="flex-end" alignItems="center">
               <ViewGrow>
-                <img width={'80%'} src={`https://oss.faycz.com/keypool/kps1m/product-2.png`}/>
+                <img width={'120%'} src={`https://oss.faycz.com/keypool/kps1m/product-2.png`}/>
               </ViewGrow>
             </Grid>
           </Grid>
